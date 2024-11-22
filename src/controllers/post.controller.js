7b4b3e6c0eeb540f8cbd433231bc019d9c7a6f01
Gustavo de Deus <88512041+GUSTAV0DEDEUS@users.noreply.github.com>
2024-11-21 22:21:12 -0300

    Update post.controller.js

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -79,7 +79,7 @@ exports.createPost = async (req, res) => {
   }
 
   try {
-
+    
     
     const student = await Student.findById(id);
     const school = await School.findById(student.school);
@@ -100,7 +100,11 @@ exports.createPost = async (req, res) => {
         school: school._id,    // ID da escola
       };
     }
-
+    
+    if (!isValidBase64(image)) {
+          return errorResponse(res, "Invalid Base64 format for profileImage.");
+    }
+      
     // Caso o usuário seja um estudante
     if (userType === "Student") {
      
@@ -134,6 +138,10 @@ exports.createPost = async (req, res) => {
   }
 };
 
+function isValidBase64(str) {
+  const base64Regex = /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/;
+  return base64Regex.test(str);
+}
 // Busca todos os posts globais e seus comentários
 exports.getGlobalPosts = async (req, res) => {
   try {
